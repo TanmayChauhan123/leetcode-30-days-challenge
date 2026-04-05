193db923dc5a93e3b92960b08b22f4cfac09fcfa
Tanmay Chauhan <tanmaychauhan2005@gmail.com>
2026-04-05 13:43:26 +0530

    Day 5 progress

---
+++ Day-05/judgeCircle.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+        int x = 0;
+        int y = 0;
+
+        for(char &ch : moves){
+
+            if(ch=='U'){
+                y++;
+            }
+
+            
+            if(ch=='L'){
+                x++;
+            }
+
+            
+            if(ch=='R'){
+                x--;
+            }
+
+            
+            if(ch=='D'){
+                y--;
+            }
+
+
+
+
+        }
+
+        return x==0 && y==0;
+        
+    }
+};
